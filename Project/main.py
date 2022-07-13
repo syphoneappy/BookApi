d1edd91a06b6578aadd070f87d97df80a8041188
syphoneappy <harshtripathi669@gmail>
2022-07-14 02:11:29 +0530

    Updated

--- Project/main.py
+++ Project/main.py
@@ -3,16 +3,16 @@ import json
 from urllib.request import urlopen
 
 app = Flask(__name__)
-
-api = "https://www.googleapis.com/books/v1/volumes?q=intitle:"
+api = "https://www.googleapis.com/books/v1/volumes?q=filter=free-ebooks:"
+apiSearch = "https://www.googleapis.com/books/v1/volumes?q=intitle:"
 
 @app.route("/")
 def covid():
-    resp = urlopen(api + "Happy")
+    resp = urlopen(api)
     book_data = json.load(resp)
-    volume_info = book_data["items"][0]["volumeInfo"]
-    author = volume_info["authors"]
-    return render_template('index.html',book_data = volume_info)
+    book =  book_data["items"]
+    Maps = map(lambda datum: datum['volumeInfo'], book)
+    return render_template('index.html',vol = Maps)
 
 
 
